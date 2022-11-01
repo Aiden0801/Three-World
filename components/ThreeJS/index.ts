adc21cd878d616f7f7415e25732884bc78edc050
Vlad <lovelypoordev@gmail.com>
2022-11-01 19:15:14 +0200

    update to typescript

---
+++ components/ThreeJS/index.ts
@@ -0,0 +1,5 @@
+export { default as Sky } from './sky';
+export { default as CameraPosition } from './CameraPosition';
+export { default as Browser } from './Browser';
+export { default as ControlPanel } from './ControlPanel'
+export { default as BrowserStatus } from './BrowserStatus'
