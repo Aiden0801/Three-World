83e2c5ec00ccfae221550185f37544cdfbd2207c
Vlad <lovelypoordev@gmail.com>
2022-11-01 22:27:30 +0200

    update to TypeScript

---
+++ types/session.type.ts
@@ -0,0 +1,9 @@
+export interface ISessionData {
+    _id?: string,
+    name?: string,
+    session_id?: string,
+    embed_url?: string,
+    creator?: string,
+    users?: object,
+    isActive?: Boolean,
+}
