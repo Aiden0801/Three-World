d83cb0e4a71c22f0aae70ce344c795f40fc8989f
Vlad <lovelypoordev@gmail.com>
2022-11-01 19:15:14 +0200

    update to typescript

---
+++ api-lib/models/users.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    browsers: [{
+        id: {
+            type: String,
+            required: false,
+        },
+        name: {
+            type: String,
+            required: false,
+        }
+    }],
+});
+
+module.exports = mongoose.models['user'] || mongoose.model('user', UserSchema);
---
+++ components/Button/index.tsx
@@ -0,0 +1,40 @@
+import { Button, } from '@mantine/core';
+import { GithubIcon, DiscordIcon, } from '@mantine/ds';
+import Image from 'next/image'
+
+export function GoogleButton(props) {
+    return <Button leftIcon={<Image src="/GoogleIcon.svg" alt="" width={36} height={36} />} variant="outline" color="black" {...props} />;
+}
+export function DiscordButton(props) {
+    return (
+        <Button
+            leftIcon={<DiscordIcon size={36} />}
+            sx={(theme) => ({
+                backgroundColor: theme.colorScheme === 'dark' ? '#5865F2' : '#5865F2',
+                '&:hover': {
+                    backgroundColor:
+                        theme.colorScheme === 'dark'
+                            ? theme.fn.lighten('#5865F2', 0.05)
+                            : theme.fn.darken('#5865F2', 0.05),
+                },
+            })}
+            {...props}
+        />
+    );
+}
+
+export function GithubButton(props) {
+    return (
+        <Button
+            {...props}
+            leftIcon={<GithubIcon size={36} />}
+            sx={(theme) => ({
+                backgroundColor: theme.colors.dark[theme.colorScheme === 'dark' ? 9 : 6],
+                color: '#fff',
+                '&:hover': {
+                    backgroundColor: theme.colors.dark[theme.colorScheme === 'dark' ? 9 : 6],
+                },
+            })}
+        />
+    );
+}
