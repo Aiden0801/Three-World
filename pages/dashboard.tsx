2bb843d6c16c1ed24a6900e0e85ea0b08fe18d9a
Aiden <lovelypoordev@gmail.com>
2022-11-04 21:47:47 -0700

    Update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Dashboard } from '../page-components/Dashboard'
-import { Navbar } from '../components/Layout'
-import { Grid } from '@mantine/core';
+import React from "react";
+import { Dashboard } from "../page-components/Dashboard";
+import { Navbar } from "../components/Layout";
+import { Grid } from "@mantine/core";
 const DashboardPage: React.FC = () => {
-    return (
-        <>
-            <Grid>
-                <Grid.Col xs={2}>
-                    <Navbar initialState={'dashboard'} /></Grid.Col>
-                <Grid.Col xs={10}> <Dashboard /></Grid.Col>
+  return (
+    <>
+      <Grid>
+        <Grid.Col xs={2}>
+          <Navbar initialState={"dashboard"} />
+        </Grid.Col>
+        <Grid.Col xs={10}>
+          {" "}
+          <Dashboard />
+        </Grid.Col>
+      </Grid>
+    </>
+  );
+};
 
-            </Grid>
-        </>
-    )
-}
-
-export default DashboardPage
+export default DashboardPage;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,13 @@
-import { getProviders, signIn } from "next-auth/react"
-import Head from 'next/head';
-import { GetServerSideProps } from 'next'
-import { Login } from '../page-components/Auth';
-export default function LogInPage(
-    // { providers }
-) {
-
-    return (
-        <>
-            <Login />
-        </>
-    )
+import { getProviders, signIn } from "next-auth/react";
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { Login } from "../page-components/Auth";
+import { Button } from "@mantine/core";
+export default function LogInPage() {
+  // { providers }
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
-
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//     const providers = await getProviders()
-//     return {
-//         props: { providers },
-//     }
-// }
