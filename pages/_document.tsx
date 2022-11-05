5917276d782730a001131e35d79c95629f38aad0
Aiden <lovelypoordev@gmail.com>
2022-11-04 21:47:47 -0700

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
-    return (
-        <Html>
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
