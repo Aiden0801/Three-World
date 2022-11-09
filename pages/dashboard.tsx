868269fead889d66e9528e43eb421b2408c2cfd5
Aiden <lovelypoordev@gmail.com>
2022-11-09 10:09:05 -0800

    fix show current page in Navbar

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,19 +2,15 @@ import React from 'react'
 import { Dashboard } from '../page-components/Dashboard'
 import { Navbar } from '../components/Layout'
 import { Grid } from '@mantine/core'
+import PageWrapper from '../page-components/utils/AppWrapper'
 const DashboardPage: React.FC = () => {
-    return (
-        <>
-            <Grid>
-                <Grid.Col xs={2}>
-                    <Navbar initialState={'dashboard'} />
-                </Grid.Col>
-                <Grid.Col xs={10}>
-                    <Dashboard />
-                </Grid.Col>
-            </Grid>
-        </>
-    )
+   return (
+      <>
+         <PageWrapper currentPage="dashboard">
+            <Dashboard />
+         </PageWrapper>
+      </>
+   )
 }
 
 export default DashboardPage
