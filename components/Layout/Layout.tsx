3a7e1d7f001346bc7ee9db0a470e9a4b823827ca
Aiden <lovelypoordev@gmail.com>
2022-11-18 11:41:23 +0200

    update

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -5,17 +5,9 @@ import { AppShell } from '@mantine/core'
 const Layout = ({ children }) => {
    return (
       <>
-<<<<<<< HEAD
-         <Container>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-         </Container>
-=======
          <AppShell padding={0} header={<Header />} footer={<Footer />}>
             {children}
          </AppShell>
->>>>>>> 801cc9e9fc256c4e528d84eb5d12645a1d5fa522
       </>
    )
 }
