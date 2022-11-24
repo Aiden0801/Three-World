cd9be47b7638ef9c12176da1b7f29bd84b7fae4d
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-11-24 10:10:33 +0200

    chagned serverURL

--- config/urlcontrol.jsx
+++ config/urlcontrol.jsx
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 export const serverURL = dev
    ? 'http://localhost:3000'
-   : 'https://three-world-iw1i3xwqm-aiden0801.vercel.app/'
+   : 'https://three-world.vercel.app'
