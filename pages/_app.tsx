4e1833ef4118906190135be5a091a07498687d39
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-11-24 09:24:03 +0200

    fix type error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
+import { ColorScheme } from '@mantine/core'
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
-import {
-   MantineProvider,
-   ColorSchemeProvider,
-   ColorScheme,
-} from '@mantine/core'
-import { wrapper } from '../store/store'
 import type { AppProps } from 'next/app'
-import React, { useState, useEffect } from 'react'
-import MySession from './_appp'
+import { useState } from 'react'
 import { RecoilRoot } from 'recoil'
+import { wrapper } from '../store/store'
+import MySession from './_appp'
 
 const root =
    typeof window !== 'undefined' && window?.document?.querySelector('#__next')
-function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
+function MyApp({
+   Component,
+   pageProps: { ...pageProps },
+}: AppProps<{
+   session: Session
+}>) {
    const [colorScheme, setColorScheme] = useState<ColorScheme>('light')
    const toggleColorScheme = (value?: ColorScheme) =>
       setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
