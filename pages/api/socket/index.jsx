505b828f341450734aa3c5c4f7a77b861a4cbce0
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-11-28 08:39:02 +0200

    test

--- pages/api/socket/index.jsx
+++ pages/api/socket/index.jsx
@@ -4,7 +4,7 @@ const SocketHandler = (req, res) => {
    if (res.socket.server.io) {
       console.log('Socket is already running')
    } else {
-      console.log('Socket is initializing', res.socket.server)
+      console.log('Socket is initializing')
 
       const io = new Server(res.socket.server)
       res.socket.server.io = io
