e3a764e8a7752382a5c0279a50ed9715e02dfc1a
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-11-29 08:17:01 +0200

    broadcast to room

--- utils/socket/messagehandler.jsx
+++ utils/socket/messagehandler.jsx
@@ -8,14 +8,14 @@ const MessageHandler = (io, socket) => {
    const participantsAdded = (msg) => {
       console.log('Participants Added Received', msg)
       socket.join(msg.sessionName)
-      socket.broadcast.emit('participantsAdded', msg)
+      socket.to(msg.sessionName).emit('participantsAdded', msg)
    }
    const disConnected = () => {
       console.log('disconnected')
    }
    const participantsRemoved = (msg) => {
       console.log('Participants Removed Message ')
-      socket.broadcast.emit('participantsRemoved', msg)
+      socket.to(msg.sessionName).emit('participantsRemoved', msg)
       socket.leave(msg.sessionName)
    }
    socket.on('participantsAdded', participantsAdded)
