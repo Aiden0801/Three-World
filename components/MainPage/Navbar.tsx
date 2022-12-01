d7ccff96d8913ef07f955c246e6a0c31c03a02d5
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-01 10:31:29 +0200

    💄 update UI

--- components/MainPage/Navbar.tsx
+++ components/MainPage/Navbar.tsx
@@ -37,11 +37,6 @@ const useStyles = createStyles((theme, _params, getRef) => {
       header: {
          paddingBottom: theme.spacing.md,
          marginBottom: theme.spacing.md * 1.5,
-         borderBottom: `1px solid ${
-            theme.colorScheme === 'dark'
-               ? theme.colors.dark[4]
-               : theme.colors.gray[2]
-         }`,
       },
       section: {
          marginLeft: -theme.spacing.md,
