ec9160854f8a17bfa34a9ff69945ef4d12770c8d
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-01 10:50:06 +0200

    🐛

--- page-components/Share/ShareScreen.tsx
+++ page-components/Share/ShareScreen.tsx
@@ -11,7 +11,7 @@ export default function ShareScreen() {
          <Suspense fallback={<LoadingOverlay visible={true} overlayBlur={2} />}>
             {/* <Space /> */}
             <ControlPanel />
-            {/* <Scene /> */}
+            <Scene />
          </Suspense>
       </>
    )
