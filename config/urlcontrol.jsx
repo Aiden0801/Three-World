beb308327fba8911052e56f488d77689e595faf7
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-07 10:58:27 +0200

    🔖 Change serverURL

--- config/urlcontrol.jsx
+++ config/urlcontrol.jsx
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 export const serverURL = dev
    ? 'http://localhost:3000'
-   : 'https://three-world.herokuapp.com'
+   : 'http://52.53.196.183:3000/'
