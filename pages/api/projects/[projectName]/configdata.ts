6086bb6d31c2729d1f63af9524a68d76273a4fcd
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-12 22:36:06 +0200

    🎨 setup endpoint

---
+++ pages/api/projects/[projectName]/configdata.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import connectMongo from '../../../../api-lib/mongodb'
+const Config = require('../../../../api-lib/models/websiteconfig')
+// ./api/session/geAvailableSession
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+   // res.status(200).json({ name: req.body, name: req.name });
+   const { projectName } = req.query
+
+   console.log(projectName)
+   await connectMongo()
+   try {
+      let project = await Config.findOne({ name: projectName }).select()
+      console.log(project)
+      let result = []
+      for (const [key, value] of Object.entries(project.template.sections)) {
+         result.push({
+            sectionName: key,
+            props: value,
+         })
+      }
+      res.status(200).send({
+         global: project.global,
+         sections: result,
+         theme: project.template.theme,
+      })
+   } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+   }
+}
+
+export default handler
