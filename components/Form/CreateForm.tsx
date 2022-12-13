78736b1f45201179e07a83661d5009f37dd28b1a
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-13 14:05:23 +0200

    🎨 added api route

--- components/Form/CreateForm.tsx
+++ components/Form/CreateForm.tsx
@@ -67,7 +67,7 @@ export const CreateFormFromConfigObject = ({
          <form
             onSubmit={form.onSubmit((values) => {
                setSubmittedValues(JSON.stringify(values, null, 2))
-               // handleOnSubmit(values)
+               handleOnSubmit(values)
             })}>
             {savedData ? (
                <></>
