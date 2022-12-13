6c82fb1eb4ba6fe1e75b48219971328d4b4c39fb
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-13 13:10:01 +0200

    🔥

--- utils/parser/globalconfig.ts
+++ utils/parser/globalconfig.ts
@@ -1,28 +1,3 @@
-const gobal_config = {
-   $schema: 'http://json-schema.org/draft-07/schema#',
-   defaultProperties: [],
-   description: 'Global common configuration for all the websites',
-   properties: {
-      description: {
-         description: 'A short description of your website, for SEO purposes',
-         title: 'description',
-         type: 'string',
-      },
-      templates: {
-         description: 'Available templates for the website.',
-         enum: ['figma'],
-         title: 'templates',
-         type: 'string',
-      },
-      title: {
-         description: 'Page title, for SEO purposes',
-         title: 'title',
-         type: 'string',
-      },
-   },
-   required: ['description', 'templates', 'title'],
-   type: 'object',
-}
 import { ParseSchema } from './schma_parser'
 import $RefParser, { JSONSchema } from '@apidevtools/json-schema-ref-parser'
 import { useEffect, useState } from 'react'
@@ -38,6 +13,7 @@ export const useGlobalConfig = (website_url: string) => {
          `${website_url}/api/config/global`
       )
       const result = ParseSchema(schema)
+
       const resultinit = getInitialValue(result)
       setInitGlobal(resultinit)
       setGlobalConfig(result)
