0a221f943783cdda4c92a61ce525fa4cbcd4ef7c
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-14 11:50:01 +0200

    🐛 change api url *getInitialValue from /api/projects/${name}/websiteconfig

--- page-components/Dashboard/Project.tsx
+++ page-components/Dashboard/Project.tsx
@@ -3,7 +3,7 @@ import { showNotification } from '@mantine/notifications'
 import { IconArrowBack, IconCheck } from '@tabler/icons'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import {  useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { CreateFormFromConfigObject } from '../../components/Form/CreateForm'
 import { LinkButton } from '../../components/Button'
@@ -12,122 +12,124 @@ import { fetcher } from '../../lib/fetcher'
 import { clientAppURL } from '../../config/urlcontrol'
 
 const fetchProjects = async (url: string, name: string) => {
-  console.log('fetch', name)
-  const data = await fetcher(`${serverURL}/api/projects/${name}/landing-page`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  })
-  return data ? data : []
+   console.log('fetch', name)
+   const data = await fetcher(
+      `${serverURL}/api/projects/${name}/websiteconfig`,
+      {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+      }
+   )
+   return data ? data : []
 }
 const useProjectData = (projectName) => {
-  console.log('use', projectName)
-  const { data, mutate, error, isValidating } = useSWR(
-    ['api/projects', projectName],
-    fetchProjects,
-    { revalidateOnFocus: false }
-  )
-  return {
-    data: data,
-    isLoading: (!error && !data) || isValidating,
-    isError: error,
-    mutate: mutate,
-  }
+   console.log('use', projectName)
+   const { data, mutate, error, isValidating } = useSWR(
+      ['api/projects', projectName],
+      fetchProjects,
+      { revalidateOnFocus: false }
+   )
+   return {
+      data: data,
+      isLoading: (!error && !data) || isValidating,
+      isError: error,
+      mutate: mutate,
+   }
 }
 const ProjectCofig = ({ projectName }) => {
-  const { data: session, status } = useSession()
-  //    const [refs, setRefs] = useState<$RefParser.$Refs>(null)
+   const { data: session, status } = useSession()
+   //    const [refs, setRefs] = useState<$RefParser.$Refs>(null)
 
-  //    const [schema, setSchema] = useState<JSONSchema>()
-  const [loaded, setLoaded] = useState(false)
-  const router = useRouter()
-  const { data: projectData } = useProjectData(projectName)
+   //    const [schema, setSchema] = useState<JSONSchema>()
+   const [loaded, setLoaded] = useState(false)
+   const router = useRouter()
+   const { data: projectData } = useProjectData(projectName)
 
-  useEffect(() => {
-    test()
-  }, [])
-  const test = async () => {
-    //   let test = await $RefParser.dereference(LandingPageSchema)
-    //   setSchema(test)
-    //   console.log(test)
-    setLoaded((o) => true)
-  }
+   useEffect(() => {
+      test()
+   }, [])
+   const test = async () => {
+      //   let test = await $RefParser.dereference(LandingPageSchema)
+      //   setSchema(test)
+      //   console.log(test)
+      setLoaded((o) => true)
+   }
 
-  const handleOnSubmit = async (values) => {
-    console.log('handleOnSubmit', values)
-    const response = await fetcher(`${serverURL}/api/projects/updateProject`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        data: values,
-      }),
-    })
-    if (response == 'Success') {
-      showNotification({
-        title: 'Success',
-        autoClose: 2000,
-        color: 'teal',
-        icon: <IconCheck size={16} />,
-        message: 'Project Configuration Updated🤥',
-      })
-    }
-  }
-  return (
-    // <Suspense fallback={<div>Loading</div>}>
+   const handleOnSubmit = async (values) => {
+      console.log('handleOnSubmit', values)
+      const response = await fetcher(
+         `${serverURL}/api/projects/updateProject`,
+         {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+               data: values,
+            }),
+         }
+      )
+      if (response == 'Success') {
+         showNotification({
+            title: 'Success',
+            autoClose: 2000,
+            color: 'teal',
+            icon: <IconCheck size={16} />,
+            message: 'Project Configuration Updated🤥',
+         })
+      }
+   }
+   return (
+      // <Suspense fallback={<div>Loading</div>}>
 
-    <Container
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <>
-        <Box
-          sx={(theme) => ({
+      <Container
+         style={{
             display: 'flex',
-            justifyContent: 'space-between',
-            align: 'center',
-            alignItems: 'center',
-            marginTop: '30px',
-          })}
-        >
-          <Text
-            component="span"
-            variant="gradient"
-            gradient={{ from: 'indigo', to: 'green', deg: 0 }}
-            weight={700}
-            style={{
-              fontFamily: 'Greycliff CF, sans-serif',
-              fontSize: '30px',
-            }}
-          >
-            {projectName}
-          </Text>
-          <LinkButton
-            compact
-            size="sm"
-            leftIcon={<IconArrowBack size={18} stroke={1.5} />}
-            color="orange"
-            pr={20}
-            href="./"
-          >
-            Back
-          </LinkButton>
-        </Box>
-        {projectData && (
-          <CreateFormFromConfigObject
-            url={clientAppURL}
-            savedData={projectData}
-            handleOnSubmit={handleOnSubmit}
-          />
-        )}
-        {projectData == undefined && (
-          <Skeleton visible={true} height={500}></Skeleton>
-        )}
-      </>
-    </Container>
-    // </Suspense>
-  )
+            justifyContent: 'center',
+            flexDirection: 'column',
+         }}>
+         <>
+            <Box
+               sx={(theme) => ({
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  align: 'center',
+                  alignItems: 'center',
+                  marginTop: '30px',
+               })}>
+               <Text
+                  component="span"
+                  variant="gradient"
+                  gradient={{ from: 'indigo', to: 'green', deg: 0 }}
+                  weight={700}
+                  style={{
+                     fontFamily: 'Greycliff CF, sans-serif',
+                     fontSize: '30px',
+                  }}>
+                  {projectName}
+               </Text>
+               <LinkButton
+                  compact
+                  size="sm"
+                  leftIcon={<IconArrowBack size={18} stroke={1.5} />}
+                  color="orange"
+                  pr={20}
+                  href="./">
+                  Back
+               </LinkButton>
+            </Box>
+            {projectData && (
+               <CreateFormFromConfigObject
+                  url={clientAppURL}
+                  savedData={projectData}
+                  handleOnSubmit={handleOnSubmit}
+               />
+            )}
+            {projectData == undefined && (
+               <Skeleton visible={true} height={500}></Skeleton>
+            )}
+         </>
+      </Container>
+      // </Suspense>
+   )
 }
 
 export default ProjectCofig
