bbb5f7d67a3944d50b0b6329aeb9d2bd968d5448
Aiden0801 <116912676+Aiden0801@users.noreply.github.com>
2022-12-14 10:57:47 +0200

    🎨 added parsing themecolor

--- lib/landing-pages/parse-schema.ts
+++ lib/landing-pages/parse-schema.ts
@@ -10,49 +10,59 @@ import * as parser from './parsers'
  * @param schema JSON Schema to parse
  */
 export function parseSchema(schema_: JSONSchema) {
-  console.info('starting parseSchema', schema_)
-  function executeParse(schema: JSONSchema) {
-    if (!schema) {
+   console.info('starting parseSchema', schema_)
+   function executeParse(schema: JSONSchema) {
+      if (!schema) {
+         return schema
+      }
+      // group 1
+
+      if (filter.isEnum(schema)) {
+         return parser.enumToSelect(schema)
+      }
+
+      if (filter.isObject(schema)) {
+         // parse the `properties` of the schema. this will return an array of
+         // objects and call recursively to parse nested schemas.
+         const fields = Object.entries(schema.properties).map(
+            ([key, value]) => {
+               const isRequired = filter.isRequired(key, schema)
+               const placeholder =
+                  value?.placeholder || value?.description || undefined
+
+               const data = {
+                  key,
+                  isRequired,
+                  placeholder,
+                  ...parser.normalizeTitle(value, 'label'),
+                  ...executeParse(value),
+               }
+               return data
+            }
+         ) // end map
+
+         return { fields, type: 'group' }
+      }
+      if (filter.isArray(schema)) {
+         // group array
+         const out = {
+            ...schema,
+            component: 'list-group',
+            items: executeParse(schema.items),
+         }
+         // TODO: sort the `items` array by some `order` property that SHOULD be in the schema
+         return out
+         // return schema.map((value) => parseSchema(value))
+      }
+      if (filter.isThemeColor(schema)) {
+         return parser.themeColor(schema)
+      }
+      // end group 1
       return schema
-    }
-    // group 1
-
-    if (filter.isEnum(schema)) {
-      return parser.enumToSelect(schema)
-    }
-
-    if (filter.isObject(schema)) {
-      // parse the `properties` of the schema. this will return an array of
-      // objects and call recursively to parse nested schemas.
-      const fields = Object.entries(schema.properties).map(([key, value]) => {
-        const isRequired = filter.isRequired(key, schema)
-        const placeholder = value?.placeholder || value?.description || undefined
-
-        const data = {
-          key,
-          isRequired,
-          placeholder,
-          ...parser.normalizeTitle(value, 'label'),
-          ...executeParse(value),
-        }
-        return data
-      }) // end map
-
-      return { fields }
-    }
-    if (filter.isArray(schema)) {
-      // group array
-      const out = { ...schema, component: 'list-group', items: executeParse(schema.items) }
-      // TODO: sort the `items` array by some `order` property that SHOULD be in the schema
-      return out
-      // return schema.map((value) => parseSchema(value))
-    }
-    // end group 1
-    return schema
-  }
-  const result = executeParse(schema_)
-  console.info('result', result)
-  return result
+   }
+   const result = executeParse(schema_)
+   console.info('result', result)
+   return result
 }
 
 // export function parseTemplate(schema: JSONSchema) {
@@ -66,4 +76,3 @@ export function parseSchema(schema_: JSONSchema) {
 //     theme: parseSchema(schema?.properties?.theme as JSONSchema),
 //   }
 // }
-
