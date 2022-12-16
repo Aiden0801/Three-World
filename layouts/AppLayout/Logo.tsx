ad18b83adef154213f5436f00e57ed5e15b77da0
dinghino <dinghino@hhhq.io>
2022-12-16 14:11:34 +0100

    💄 responsive HHHQ Title in header
    now shrinks to just HHHQ on < sm to fit properly

--- layouts/AppLayout/Logo.tsx
+++ layouts/AppLayout/Logo.tsx
@@ -1,9 +1,15 @@
-import { Image, Text, Title } from "@mantine/core";
+import { Image, MediaQuery, Text, Title } from "@mantine/core";
 
 export function TextLogo() {
   return (
     <Title order={2}>
-      HackerHouse
+      <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+        <Text component="span">HackerHouse</Text>
+      </MediaQuery>
+      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+        <Text component="span">HH</Text>
+      </MediaQuery>
+
       <Text span size="xl" color="green" inherit>
         HQ
       </Text>
