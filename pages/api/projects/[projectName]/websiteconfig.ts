22f4d88e841b22032d47e8b70ed3af39f7fbcac7
dinghino <dinghino@hhhq.io>
2022-12-17 15:37:37 +0100

    🧑‍💻 example usage for pino logger

--- pages/api/projects/[projectName]/websiteconfig.ts
+++ pages/api/projects/[projectName]/websiteconfig.ts
@@ -1,21 +1,23 @@
 import axios from 'axios'
-import connectMongo from '../../../../api-lib/mongodb'
-const Config = require('../../../../api-lib/models/websiteconfig')
+import connectMongo from '@/api-lib/mongodb'
+
+const Config = require('@/api-lib/models/websiteconfig')
+
+import logger from '@/utils/logger'
 // ./api/session/geAvailableSession
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   // res.status(200).json({ name: req.body, name: req.name });
   const { projectName } = req.query
-
-  console.log(projectName)
-  await connectMongo()
+  logger.debug(`requesting ${projectName}`)
   try {
+    await connectMongo()
     let project = await Config.findOne({ slug: projectName }).select()
-    console.log(project)
+    logger.debug('project found')
     res.status(200).send(project)
   } catch (err) {
-    console.error(err.message)
+    logger.error(err.message)
     res.status(500).send('Server error')
   }
 }
