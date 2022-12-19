081838e111db5bdda7345ec331181beb8d93f579
dinghino <dinghino@hhhq.io>
2022-12-19 18:39:35 +0100

    🏷️ improve typings for landing pages

--- lib/landing-pages/types.ts
+++ lib/landing-pages/types.ts
@@ -1,4 +1,7 @@
-export type FormFieldComponentType =
+/**
+ * @deprecated in favor of dynamic ComponentType below
+ */
+type _FormFieldComponentType =
   /** this should be the default type */
   | 'text'
   /** number inputs should be handled differently (NumberInput component) */
@@ -27,3 +30,55 @@ export type FormFieldComponentType =
    * fields for each item in the array
    */
   | 'list-group'
+
+export type GenericFormField = {
+  /** label | title for the input component */
+  label: string
+  /** placeholder text for the inputs */
+  placeholder?: string
+  /** true if the field is in the `required` list of properties of the schema */
+  isRequired?: boolean
+  /** original JSON schema type */
+  type: string
+  // component: FormFieldComponentType // TODO: This is one of our components
+}
+
+export type TextFormField = GenericFormField & {
+  component: 'text'
+}
+
+export type NumberFormField = GenericFormField & {
+  component: 'number'
+}
+
+export type ListGroupFormField = GenericFormField & {
+  component: 'list-group'
+  items: AnyFormField[]
+}
+export type CheckboxFormField = GenericFormField & {
+  component: 'checkbox'
+  items: AnyFormField[]
+}
+
+export type LinkGroupFormField = GenericFormField & {
+  component: 'link-group'
+  items: AnyFormField[]
+}
+
+export type SelectFormField = GenericFormField & {
+  component: 'select'
+  // these comes from mantine's Select component
+  data: string[] | { label: string; value: string }[]
+}
+
+export type FormComponentType = AnyFormField['component']
+
+export type AnyFormField =
+  | TextFormField
+  | LinkGroupFormField
+  | SelectFormField
+  | ListGroupFormField
+  | NumberFormField
+  | CheckboxFormField
+
+export type FormField<T extends any> = T extends FormComponentType ? Extract<AnyFormField, { component: T }> : AnyFormField
