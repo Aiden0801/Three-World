df3a356e8fa43fc2ff0640908e3e02e5ae668165
dinghino <dinghino@gmail.com>
2022-12-24 15:14:55 +0100

    💄 hide login button

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
         {/* <meta name="twitter:card" content="summary" />
